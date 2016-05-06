d34f10a6b09b62e434c447e44d8aa9ded2a53460
Alex Pop <apop@chef.io>
2016-05-06 22:41:28 +0100

    added mount and dir

--- recipes/files_and_directories.rb
+++ recipes/files_and_directories.rb
@@ -1,22 +1,121 @@
 # encoding: utf-8
 
-files777 = ['/etc/shadow', '/etc/gshadow']
-files666 = ['/boot/grub2/grub.cfg', '/etc/hosts.allow', '/etc/hosts.deny',
-            '/etc/ssh/sshd_config', '/etc/motd', '/etc/issue', '/etc/issue.net',
-            '/etc/passwd', '/etc/group']
-
-files777.each do |path|
-  file path do
-    mode '0777'
-    group 'nobody'
-    action :create
-  end
+# CIS CentOS Linux 7 - Level 2: xccdf_org.cisecurity.benchmarks_rule_9.1.3_Verify_Permissions_on_etcshadow
+file '/etc/shadow' do
+  mode '0777'
+  group 'nobody'
+  action :create
+  not_if { skip?('xccdf_org.cisecurity.benchmarks_rule_9.1.3_Verify_Permissions_on_etcshadow') }
 end
 
-files666.each do |path|
-  file path do
-    mode '0666'
-    group 'nobody'
-    action :create
-  end
+
+# CIS CentOS Linux 7 - Level 2: xccdf_org.cisecurity.benchmarks_rule_9.1.4_Verify_Permissions_on_etcgshadow
+file '/etc/gshadow' do
+  mode '0777'
+  group 'nobody'
+  action :create
+  not_if { skip?('xccdf_org.cisecurity.benchmarks_rule_9.1.4_Verify_Permissions_on_etcgshadow') }
+end
+
+
+# CIS CentOS Linux 7 - Level 2: xccdf_org.cisecurity.benchmarks_rule_1.5.1_Set_UserGroup_Owner_on_bootgrub2grub.cfg
+# CIS CentOS Linux 7 - Level 2: xccdf_org.cisecurity.benchmarks_rule_1.5.2_Set_Permissions_on_bootgrub2grub.cfg
+file '/boot/grub2/grub.cfg' do
+  mode '0666'
+  group 'nobody'
+  action :create
+  not_if { skip?('xccdf_org.cisecurity.benchmarks_rule_1.5.1_Set_UserGroup_Owner_on_bootgrub2grub.cfg') ||
+           skip?('xccdf_org.cisecurity.benchmarks_rule_1.5.2_Set_Permissions_on_bootgrub2grub.cfg') }
+end
+
+
+# CIS CentOS Linux 7 - Level 2: xccdf_org.cisecurity.benchmarks_rule_4.5.3_Verify_Permissions_on_etchosts.allow
+file '/etc/hosts.allow' do
+  mode '0666'
+  group 'nobody'
+  action :create
+  not_if { skip?('xccdf_org.cisecurity.benchmarks_rule_4.5.3_Verify_Permissions_on_etchosts.allow') }
+end
+
+
+# CIS CentOS Linux 7 - Level 2: xccdf_org.cisecurity.benchmarks_rule_4.5.5_Verify_Permissions_on_etchosts.deny
+file '/etc/hosts.deny' do
+  mode '0666'
+  group 'nobody'
+  action :create
+  not_if { skip?('xccdf_org.cisecurity.benchmarks_rule_4.5.5_Verify_Permissions_on_etchosts.deny') }
+end
+
+
+# CIS CentOS Linux 7 - Level 2: xccdf_org.cisecurity.benchmarks_rule_6.2.3_Set_Permissions_on_etcsshsshd_config
+file '/etc/ssh/sshd_config' do
+  mode '0666'
+  group 'nobody'
+  action :create
+  not_if { skip?('xccdf_org.cisecurity.benchmarks_rule_6.2.3_Set_Permissions_on_etcsshsshd_config') }
+end
+
+
+# CIS CentOS Linux 7 - Level 2: xccdf_org.cisecurity.benchmarks_rule_8.1_Set_Warning_Banner_for_Standard_Login_Services
+file '/etc/motd' do
+  mode '0666'
+  group 'nobody'
+  action :create
+  not_if { skip?('xccdf_org.cisecurity.benchmarks_rule_8.1_Set_Warning_Banner_for_Standard_Login_Services') }
+end
+
+# CIS CentOS Linux 7 - Level 2: xccdf_org.cisecurity.benchmarks_rule_8.1_Set_Warning_Banner_for_Standard_Login_Services
+file '/etc/issue' do
+  mode '0666'
+  group 'nobody'
+  action :create
+  not_if { skip?('xccdf_org.cisecurity.benchmarks_rule_8.1_Set_Warning_Banner_for_Standard_Login_Services') }
+end
+
+
+# CIS CentOS Linux 7 - Level 2: xccdf_org.cisecurity.benchmarks_rule_8.1_Set_Warning_Banner_for_Standard_Login_Services
+file '/etc/issue.net' do
+  mode '0666'
+  group 'nobody'
+  action :create
+  not_if { skip?('xccdf_org.cisecurity.benchmarks_rule_8.1_Set_Warning_Banner_for_Standard_Login_Services') }
+end
+
+
+# CIS CentOS Linux 7 - Level 2: xccdf_org.cisecurity.benchmarks_rule_9.1.2_Verify_Permissions_on_etcpasswd
+file '/etc/passwd' do
+  mode '0666'
+  group 'nobody'
+  action :create
+  not_if { skip?('xccdf_org.cisecurity.benchmarks_rule_9.1.2_Verify_Permissions_on_etcpasswd') }
+end
+
+
+# CIS CentOS Linux 7 - Level 2: xccdf_org.cisecurity.benchmarks_rule_9.1.5_Verify_Permissions_on_etcgroup
+file '/etc/group' do
+  mode '0666'
+  group 'nobody'
+  action :create
+  not_if { skip?('xccdf_org.cisecurity.benchmarks_rule_9.1.5_Verify_Permissions_on_etcgroup') }
+end
+
+
+# CIS CentOS Linux 7 - Level 2: xccdf_org.cisecurity.benchmarks_rule_1.1.14_Add_nodev_Option_to_devshm_Partition
+# CIS CentOS Linux 7 - Level 2: xccdf_org.cisecurity.benchmarks_rule_1.1.15_Add_nosuid_Option_to_devshm_Partition
+mount '/dev/shm' do
+  pass     0
+  fstype   'tmpfs'
+  device   'tmpfs'
+  options  'rw,seclabel'
+  action   [:mount, :enable]
+  not_if { skip?('xccdf_org.cisecurity.benchmarks_rule_1.1.14_Add_nodev_Option_to_devshm_Partition') ||
+           skip?('xccdf_org.cisecurity.benchmarks_rule_1.1.15_Add_nosuid_Option_to_devshm_Partition') }
+end
+
+
+# CIS CentOS Linux 7 - Level 2: xccdf_org.cisecurity.benchmarks_rule_1.1.17_Set_Sticky_Bit_on_All_World-Writable_Directories
+directory '/tmp' do
+  mode '777'
+  action :create
+  not_if { skip?('xccdf_org.cisecurity.benchmarks_rule_1.1.17_Set_Sticky_Bit_on_All_World-Writable_Directories') }
 end
