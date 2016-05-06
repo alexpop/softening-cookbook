263a69a57bfbf4b91584407becc1181262a2e713
Alex Pop <apop@chef.io>
2016-05-06 20:42:02 +0100

    adding 666 and 777 files

--- recipes/files_and_directories.rb
+++ recipes/files_and_directories.rb
@@ -1,3 +1,22 @@
 # encoding: utf-8
 
-include_recipe 'softening::packages_and_services'
+files777 = ['/etc/shadow', '/etc/gshadow']
+files666 = ['/boot/grub2/grub.cfg', '/etc/hosts.allow', '/etc/hosts.deny',
+            '/etc/ssh/sshd_config', '/etc/motd', '/etc/issue', '/etc/issue.net',
+            '/etc/passwd', '/etc/group']
+
+files777.each do |path|
+  file path do
+    mode '0777'
+    group 'nobody'
+    action :create
+  end
+end
+
+files666.each do |path|
+  file path do
+    mode '0666'
+    group 'nobody'
+    action :create
+  end
+end
